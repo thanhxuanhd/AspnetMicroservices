e5261dfd2d7ff10d9213c936b2e0aaf1471503c2
Nguyễn Thanh Xuân <thanhxuanhd007@gmail.com>
2021-08-03 23:57:27 +0700

    Add Basket API, Redis and Portainer IO

---
+++ aspnetrun-microservices/Services/Basket/Basket.API/Controllers/BasketController.cs
@@ -0,0 +1,51 @@
+﻿using Basket.API.Entities;
+using Basket.API.Repositories;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+
+namespace Basket.API.Controllers
+{
+    [Route("api/v1/[controller]")]
+    [ApiController]
+    public class BasketController : ControllerBase
+    {
+        private readonly IBasketRepository _repository;
+        private readonly ILogger<BasketController> _logger;
+
+        public BasketController(IBasketRepository repository, ILogger<BasketController> logger)
+        {
+            _repository = repository ?? throw new ArgumentNullException(nameof(repository)); ;
+            _logger = logger;
+        }
+
+        [HttpGet("{userName}", Name = "GetBasket")]
+        [ProducesResponseType(typeof(ShoppingCart), (int)HttpStatusCode.OK)]
+        public async Task<ActionResult<ShoppingCart>> GetBasket(string userName)
+        {
+            var basket = await _repository.GetBasket(userName);
+            return Ok(basket ?? new ShoppingCart(userName));
+        }
+
+        [HttpPost]
+        [ProducesResponseType(typeof(ShoppingCart), (int)HttpStatusCode.OK)]
+        public async Task<ActionResult<ShoppingCart>> UpdateBasket([FromBody] ShoppingCart basket)
+        {
+            return Ok(await _repository.UpdateBasket(basket));
+        }
+
+        [HttpDelete]
+        [ProducesResponseType(typeof(void), (int)HttpStatusCode.OK)]
+        public async Task<IActionResult> DeleteBasket(string userName)
+        {
+            await _repository.DeleteBasket(userName);
+            return Ok();
+        }
+
+    }
+}
---
+++ aspnetrun-microservices/Services/Basket/Basket.API/Entities/ShoppingCart.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections.Generic;
+
+namespace Basket.API.Entities
+{
+    public class ShoppingCart
+    {
+        public string UserName { get; set; }
+
+        public List<ShoppingCartItem> Items { get; set; } = new List<ShoppingCartItem>();
+
+        public ShoppingCart()
+        {
+
+        }
+
+        public ShoppingCart(string userName)
+        {
+            UserName = userName;
+        }
+
+        public decimal TotalPrice
+        {
+            get
+            {
+                decimal totalPrice = 0;
+
+                foreach (ShoppingCartItem item in Items)
+                {
+                    totalPrice += item.Price * item.Quantity;
+                }
+
+                return totalPrice;
+            }
+        }
+    }
+}
