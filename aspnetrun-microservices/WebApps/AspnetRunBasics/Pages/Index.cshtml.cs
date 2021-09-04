2e8e1ffcd3d8585ab61aad6c5dbfcc4a6030ae81
Nguyễn Thanh Xuân <thanhxuanhd007@gmail.com>
2021-09-04 22:16:19 +0700

    Refactor Code

--- aspnetrun-microservices/WebApps/AspnetRunBasics/Pages/Index.cshtml.cs
+++ aspnetrun-microservices/WebApps/AspnetRunBasics/Pages/Index.cshtml.cs
@@ -1,7 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Threading.Tasks;
-using AspnetRunBasics.Models;
+﻿using AspnetRunBasics.Models;
 using AspnetRunBasics.Services;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
